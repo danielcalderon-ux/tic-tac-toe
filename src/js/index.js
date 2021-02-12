895802ff55ec3b7d5431fa6920cca9b312a9b8e2
danielcalderon-ux <danielcal89@hotmail.com>
2021-02-12 03:09:06 +0000

    nuevo

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,8 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { Game } from "./component/Board.js";
+import { Board } from "./component/Board.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Game />, document.querySelector("#app"));
